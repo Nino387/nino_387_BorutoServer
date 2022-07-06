5779698e84524908b5825c3a89ac946267b3dc8a
nliporazzi <nino.liporazzi@mercadolibre.com>
2022-07-06 19:20:33 -0300

    SetUp - initial commit

---
+++ src/main/kotlin/com/backend/Application.kt
@@ -0,0 +1,15 @@
+package com.backend
+
+import io.ktor.server.application.*
+import com.backend.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureKoin()
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
---
+++ src/main/kotlin/com/backend/plugins/Koin.kt
@@ -0,0 +1,8 @@
+package com.backend.plugins
+
+import io.ktor.server.application.*
+import org.koin.ktor.ext.Koin
+
+fun Application.configureKoin(){
+
+}
